8aeba2f363171625fe469d7f6a52662757ac74a8
szhou1 <zhousteve3@gmail.com>
2013-06-22 23:38:49 -0500

    damn git

--- src/com/ufit/activity/MainActivity.java
+++ src/com/ufit/activity/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
 
 		addListenerOnProgressButton();
 		addListenerOnDietButton();
-		addListenerOnCalendarButton();//aklsdjflkafjlkfj
+		addListenerOnCalendarButton();
 	}
 
 	public void addListenerOnProgressButton() {
